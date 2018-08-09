92cf0d4e30c77c2a4761abc03e144408dade76d4
Susan Rolfes <rolfes57069@yahoo.com>
2018-08-09 20:40:45 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,14 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz (x)
+ if x % 3 == 0 && x % 5 == 0
+   puts "FizzBuzz"
+ elsif x % 3 == 0
+    puts "Fizz"
+  elsif x % 5 == 0
+    puts "Buzz"
+  else
+    puts "nil"
+ end
+ fizzbuzz (15)
+end
